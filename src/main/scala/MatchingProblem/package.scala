c2aafafae0dadb5efb4bb1acc076b24fdcdb4749
Nicolas Herrera <herreran903@gmail.com>
2023-05-01 12:47:18 -0500

    Commit 1

--- src/main/scala/MatchingProblem/package.scala
+++ src/main/scala/MatchingProblem/package.scala
@@ -1,3 +1,23 @@
-package object MatchingProblem {
+package object MatchingProblem
+{
+  /** ****************************************************************************
+   * Funciones Entregadas por el profesor.
+   * **************************************************************************** */
+  type Match = (Int, Int)
+  type Matching = List[Match]
+  type Preferences = Vector[Vector[Int]]
 
+  /** ****************************************************************************
+   * FUNCIÓN:                matchByElement
+   * DESCRIPCIÓN:            Función que retorna todas las posibles combinaciones entre el piloto i con n copilotos.
+   * PARÁMETROS DE ENTRADA
+   * $i :                    Numero del piloto.
+   * $n :                    Numero de copilotos.
+   * RETORNO
+   * List[Match] :  			   Todas las posibles combinaciones entre el piloto i con n copilotos.
+   * **************************************************************************** */
+  def matchByElement (i:Int, n:Int): List[Match] =
+  {
+
+  }
 }
